0094f70a99420c39f99626f3c1fa1e56f18527b8
HelHeim111 <dwp2004@icloud.com>
2022-10-21 17:23:01 +0200

    Added token structure, function Check_keyWord, started working on scanning

--- scanner.c
+++ scanner.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <scanner.h>
+
+int Check_Keyword(char *s){
+    for(int i = 0; i < 10; i++){
+        if(strcmp(s, keywords[i])){
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int main (int argc, char* argv[]) {
+    char c; //responsible for reading each character
+    int state = 0; //states of finite-state machine
+    Token_t token[];
+    int tokenCounter = 0; //ordinal number of the token
+    while((c = getchar()) != EOF){
+        switch (state){
+            case 0:
+                if(c == '$'){
+                    state = 1;
+                    strncat(token[tokenCounter].text, c, 1); //appending the text of token 
+                }
+                if(isdigit(c)){
+                    state = 3;
+                }
+            case 1:
+                if(c == '_' || isalfa(c)){
+                    state = 2;
+                }
+            case 2:
+                if(c == '_' || isalfa(c) || isdigit(c)){
+                    token[tokenCounter].type = VARIABLE;
+                }
+        }
+    }
     return 0;
 }
--- scanner.h
+++ scanner.h
@@ -11,6 +11,27 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+#define ELSE 256
+#define FLOAT 257
+#define FUNCTION 258
+#define IF 259
+#define INT 260
+#define NULL 261
+#define RETURN 262
+#define STRING 263
+#define VOID 264
+#define WHILE 265
+#define VARIABLE 266
+//all the keywords we have
+char keywords[10][10] = {"else", "if", "float", "function", "int", "null", "return", "string", "void", "while"};
 
+typedef struct Token{
+    size_t pos; //position in code 
+    int type;
+    char *text; 
+}Token_t;
+
+int Check_Keyword(char *s);
+void Scan_Word(File* f, Token_t token);
 
 #endif
