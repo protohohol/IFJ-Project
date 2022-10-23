7ce89fd00cce7fe2edb4ef845a6e93fd59b8b898
HelHeim111 <dwp2004@icloud.com>
2022-10-23 02:31:20 +0200

    Added enum with token types

--- scanner.h
+++ scanner.h
@@ -1,3 +1,30 @@
+
+Skip to content
+Pull requests
+Issues
+Marketplace
+Explore
+@HelHeim111
+protohohol /
+IFJ-Project
+Private
+
+Code
+Issues
+Pull requests
+Actions
+Projects 1
+Security
+
+    Insights
+
+IFJ-Project/scanner.h
+@protohohol
+protohohol test_0.3
+Latest commit c299509 yesterday
+History
+1 contributor
+executable file 39 lines (33 sloc) 680 Bytes
 /*
 * IFJ/IAL project 2022.
 * 
@@ -37,3 +64,20 @@ int Check_Keyword(char *s);
 void Scan_Word(FILE* f, Token_t token);
 
 #endif
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
+IFJ-Project/scanner.h at develop · protohohol/IFJ-Project
