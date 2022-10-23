505eb431b6d35bb2c7e47f50090b637932245e6a
protohohol <pinlog33@gmail.com>
2022-10-22 23:17:50 -0400

    minor changes

--- scanner.c
+++ scanner.c
@@ -12,16 +12,16 @@ int Check_Keyword(char *s){
 }
  
 int main (int argc, char* argv[]) {
-    char c; //responsible for reading each character
-    int state = 0; //states of finite-state machine
+    char c; // responsible for reading each character
+    int state = 0; // states of finite-state machine
     Token_t token[1];
-    int tokenCounter = 0; //ordinal number of the token
+    int tokenCounter = 0; // ordinal number of the token
     while((c = getchar()) != -1){
         switch (state){
             case 0:
                 if(c == '$'){
                     state = 1;
-                    strncat(token[tokenCounter].text, c, 1); //appending the text of token 
+                    strncat(token[tokenCounter].text, c, 1); // appending the text of token 
                 }
                 if(isdigit(c)){
                     state = 3;
@@ -32,7 +32,7 @@ int main (int argc, char* argv[]) {
                 }
             case 2:
                 if(c == '_' || isalpha(c) || isdigit(c)){
-                    token[tokenCounter].type = VARIABLE;
+                    // token[tokenCounter].type = VARIABLE;
                 }
         }
     }
--- scanner.h
+++ scanner.h
@@ -1,33 +1,34 @@
-/*
-* IFJ/IAL project 2022.
-* 
-* Authors: 
-*   Vsevolod Tiemnohorov(xtiemn00)
-*   xxx
-*   xxx
-*   xxx
-*/
+/**
+ * @file scanner.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
 #ifndef SCANNER_H
 #define SCANNER_H
 
 #include <stdio.h>
 
-//all types for tokens
+// all types for tokens
 enum TokenType{
-    STAR, STAR_STAR, SLASH, PLUS, MINUS, DOT, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
-    BRACE_LEFT, BRACE_RIGHT, BRACKET_LEFT, BRACKET_RIGHT, SEMICOLON
-    EQUAL, EQUAL_EQUAL_EQUAL, NOT_EQUAL_EQUAL,
+    T_STAR=256, T_STAR_STAR, T_SLASH, T_PLUS, T_MINUS, T_DOT, T_GREATER, T_GREATER_EQUAL, T_LESS, T_LESS_EQUAL,
+    T_BRACE_LEFT, T_BRACE_RIGHT, T_BRACKET_LEFT, T_BRACKET_RIGHT, T_SEMICOLON,
+    T_EQUAL, T_EQUAL_EQUAL_EQUAL, T_NOT_EQUAL_EQUAL,
 
-    ID, INT, FLOAT, DOUBLE, STRING
+    T_ID, T_INT, T_FLOAT, T_DOUBLE, T_STRING,
 
-    ELSE, IF, FLOAT_K, FUNCTION, INT_K, NULL, RETURN, STRING_K, VOID, WHILE
+    T_ELSE, T_IF, T_FLOAT_K, T_FUNCTION, T_INT_K, T_NULL, T_RETURN, T_STRING_K, T_VOID, T_WHILE
 };
-//all the keywords we have
+// all the keywords we have
 char keywords[10][10] = {"else", "if", "float", "function", "int", "null", "return", "string", "void", "while"};
 
 typedef struct Token {
-    size_t pos; //position in code 
+    size_t pos; // position in code 
     enum TokenType type;
     char *text; 
 } Token_t;
