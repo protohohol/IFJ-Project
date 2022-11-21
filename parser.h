00accd2d3aa8bdb572f14a46de8fc9edb1ce1a45
Sviatoslav <atomol112@gmail.com>
2022-11-21 21:05:09 +0100

    some theory and staff

--- parser.h
+++ parser.h
@@ -3,5 +3,8 @@
 #include "scanner.h"
 #include "error.h"
 
-int st_list(token_t * token);
+int st_list ( token_t * token );
+int define ( token_t * token );
+int f_list ( token_t * token );
+int declare ( token_t * token );
 #endif
