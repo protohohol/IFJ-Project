e81470af9f1ef57f586326d3403d5df98c8831ae
HelHeim111 <dwp2004@icloud.com>
2022-12-02 20:45:13 +0100

    shit commit v1

--- code_generator.h
+++ code_generator.h
@@ -8,11 +8,7 @@
 #include "symbol_table.h"
 #include "str.h"
 
-typedef enum {
-    F_GF,   //global frame
-    F_LF,   //local frame
-    F_TF,   //temporary frame
-}frame_type;
+
 
 typedef enum {
     I_MOVE,             
@@ -85,71 +81,7 @@ typedef enum {
 
 }instructions;
 
-typedef struct {
-    string value;
-    token_type type;
-    frame_type frame;
-    bool tmp;
-    bool local;
-    bool label;
-}operator_t;
 
-typedef struct {
-    void* operant_1;
-    void* operant_2;
-    void* result;
-    operator_t* operator;
-}taCode;
-
-typedef struct DLLElement{
-    taCode data;
-    struct DLLElement *previousElement;
-    struct DLLElement *nextElement;
-}*DLLElementPtr;
-
-typedef struct {
-    DLLElementPtr firstElement;
-    DLLElementPtr lastElement;
-    DLLElementPtr activeElement;
-}DLList;
-
-void DLL_Init( DLList* list);
-
-void DLL_Dispose( DLList* list);
-
-void DLL_InsertFirst( DLList* list, taCode data);
-
-void DLL_InsertLast( DLList* list, taCode data);
-
-void DLL_First( DLList* list);
-
-void DLL_Last( DLList* list);
-
-void DLL_GetFirst( DLList* list, taCode* dataPtr);
-
-void DLL_GetLast( DLList* list, taCode* dataPtr);
-
-void DLL_DeleteFirst( DLList* list);
-
-void DLL_DeleteLast( DLList* list);
-
-void DLL_DeleteAfter( DLList* list);
-
-void DLL_DeleteBefore( DLList* list);
-
-void DLL_InsertAfter( DLList* list, taCode data);
-
-void DLL_InsertBefore( DLList* list, taCode data);
-
-void DLL_GetValue( DLList* list, taCode* dataPtr);
-
-void DLL_SetValue( DLList* list, taCode data);
-
-void DLL_Next( DLList* list);
-
-void DLL_Previous( DLList* list);
-
-int DLL_IsActive( DLList* list);
 
 
 
---
+++ instruction_list.h
@@ -0,0 +1,83 @@
+#ifndef INSTRUCTION_LIST
+#define INSTRUCTION_LIST
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "scanner.h"
+#include "symbol_table.h"
+#include "str.h"
+
+typedef enum {
+    F_GF,   //global frame
+    F_LF,   //local frame
+    F_TF,   //temporary frame
+}frame_type;
+
+typedef struct {
+    string value;
+    token_type type;
+    frame_type frame;
+    bool tmp;
+    bool local;
+    bool label;
+}operator_t;
+
+typedef struct {
+    void* operand_1;
+    void* operand_2;
+    void* result;
+    operator_t operator;
+}taCode;
+
+typedef struct DLLElement{
+    taCode data;
+    struct DLLElement *previousElement;
+    struct DLLElement *nextElement;
+}*DLLElementPtr;
+
+typedef struct {
+    DLLElementPtr firstElement;
+    DLLElementPtr lastElement;
+    DLLElementPtr activeElement;
+}DLList;
+
+void DLL_Init( DLList* list);
+
+void DLL_Dispose( DLList* list);
+
+void DLL_InsertFirst( DLList* list, taCode data);
+
+void DLL_InsertLast( DLList* list, taCode data);
+
+void DLL_First( DLList* list);
+
+void DLL_Last( DLList* list);
+
+void DLL_GetFirst( DLList* list, taCode* dataPtr);
+
+void DLL_GetLast( DLList* list, taCode* dataPtr);
+
+void DLL_DeleteFirst( DLList* list);
+
+void DLL_DeleteLast( DLList* list);
+
+void DLL_DeleteAfter( DLList* list);
+
+void DLL_DeleteBefore( DLList* list);
+
+void DLL_InsertAfter( DLList* list, taCode data);
+
+void DLL_InsertBefore( DLList* list, taCode data);
+
+void DLL_GetValue( DLList* list, taCode* dataPtr);
+
+void DLL_SetValue( DLList* list, taCode data);
+
+void DLL_Next( DLList* list);
+
+void DLL_Previous( DLList* list);
+
+int DLL_IsActive( DLList* list);
+
+#endif
