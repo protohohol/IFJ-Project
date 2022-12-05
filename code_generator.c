7bc492c43e2b026e34a63849a399314fd0743a4a
HelHeim111 <dwp2004@icloud.com>
2022-12-05 20:55:28 +0100

    minor changes

--- code_generator.c
+++ code_generator.c
@@ -3,9 +3,52 @@
 
 void codeStart(DLList* list) {
 	printf(".IFJcode22\n\
+	DEFVAR GF@tmp1\n\
+	DEFVAR GF@tmp2\n\
+	DEFVAR GF@tmp3\n\
 	JUMP main\n\
+	\n\
+	LABEL readi\n\
+		PUSHFRAME\n\
+		DEFVAR LF@VAR\n\
+		READ LF@VAR int\n\
+		POPFRAME\n\
+		RETURN\n\
+	\n\
+	LABEL readf\n\
+		PUSHFRAME\n\
+		DEFVAR LF@VAR\n\
+		READ LF@VAR float\n\
+		POPFRAME\n\
+		RETURN\n\
+	\n\
+	LABEL reads\n\
+		PUSHFRAME\n\
+		DEFVAR LF@VAR\n\
+		READ LF@VAR string\n\
+		POPFRAME\n\
+		RETURN\n\
+	\n\
+	LABEL write\n\
+		LABLE write$loop\n\
+		POPS GF@tmp1\n\
+		JUMPIFEQ write$end GF@tmp1 nil@nil\n\
+		WRITE GF@tmp1\n\
+		JUMP write$loop\n\
+		LABLE write$end\n\
+	\n\
+	LABEL floatval\n\
+		POPS GF@tmp1\n\
+		DEFVAR LF@varType\n\
+		DEFVAR LF@return\n\
+		TYPE LF@varType GF@tmp1\n\
+		JUMPIFEQ floatval$nil LF@varType string@nil\n\
+		JUMPIFEQ floatval$int LF@varType string@int\n\
+		JUMPIFEQ floatval$float LF@varType string@float\n\
+		JUMP floatval$end\n\
+		\n\
+		LABEL floatval$nil\n\
 	");
-	//todo: standart functions
 }
 
 void printInstruction(DLList* list) {
