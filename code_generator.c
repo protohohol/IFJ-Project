52b52b6dfee8286d4da0056c9c51bb3e8636ebfa
HelHeim111 <dwp2004@icloud.com>
2022-12-06 03:39:46 +0100

    added almost all default functions

--- code_generator.c
+++ code_generator.c
@@ -6,48 +6,128 @@ void codeStart(DLList* list) {
 	DEFVAR GF@tmp1\n\
 	DEFVAR GF@tmp2\n\
 	DEFVAR GF@tmp3\n\
-	JUMP main\n\
+	JUMP $$main\n\
 	\n\
-	LABEL readi\n\
+	LABEL $readi\n\
 		PUSHFRAME\n\
 		DEFVAR LF@VAR\n\
 		READ LF@VAR int\n\
 		POPFRAME\n\
 		RETURN\n\
 	\n\
-	LABEL readf\n\
+	LABEL $readf\n\
 		PUSHFRAME\n\
 		DEFVAR LF@VAR\n\
 		READ LF@VAR float\n\
 		POPFRAME\n\
 		RETURN\n\
 	\n\
-	LABEL reads\n\
+	LABEL $reads\n\
 		PUSHFRAME\n\
 		DEFVAR LF@VAR\n\
 		READ LF@VAR string\n\
 		POPFRAME\n\
 		RETURN\n\
 	\n\
-	LABEL write\n\
-		LABLE write$loop\n\
-		POPS GF@tmp1\n\
-		JUMPIFEQ write$end GF@tmp1 nil@nil\n\
-		WRITE GF@tmp1\n\
-		JUMP write$loop\n\
-		LABLE write$end\n\
+	LABEL $floatval\n\
+		PUSHFRAME\n\
+		DEFVAR LF@$floatval$return\n\
+		MOVE LF@$floatval$return LF@%0\n\
+		TYPE GF@tmp1 LF@%0\n\
+		JUMPIFEQ $floatval$type$int GF@tmp1 string@int\n\
+		JUMPIFEQ $floatval$type$nil GF@tmp1 string@nil\n\
+		JUMP $floatval$end\n\
+		LABEL $floatval$type$int\n\
+		INT2FLOAT LF@$floatval$return LF@%0\n\
+		JUMP $floatval$end\n\
+		LABEL $floatval$type$nil\n\
+		INT2FLOAT LF@$floatval$return int@0\n\
+		LABEL $floatval$end\n\
+		POPFRAME\n\
+		RETURN\n\
 	\n\
-	LABEL floatval\n\
-		POPS GF@tmp1\n\
-		DEFVAR LF@varType\n\
-		DEFVAR LF@return\n\
-		TYPE LF@varType GF@tmp1\n\
-		JUMPIFEQ floatval$nil LF@varType string@nil\n\
-		JUMPIFEQ floatval$int LF@varType string@int\n\
-		JUMPIFEQ floatval$float LF@varType string@float\n\
-		JUMP floatval$end\n\
-		\n\
-		LABEL floatval$nil\n\
+	LABEL $intval\n\
+		PUSHFRAME\n\
+		DEFVAR LF@$intval$return\n\
+		MOVE LF@$intval$return LF@%0\n\
+		TYPE GF@tmp1 LF@%0\n\
+		JUMPIFEQ $intval$type$float GF@tmp1 string@float\n\
+		JUMPIFEQ $intval$type$nil GF@tmp1 string@nil\n\
+		JUMP $intval$end\n\
+		LABEL $intval$type$float\n\
+		FLOAT2INT LF@$intval$return LF@%0\n\
+		JUMP $intval$end\n\
+		LABEL $intval$type$nil\n\
+		MOVE LF@$intval$return int@0\n\
+		LABEL $intval$end\n\
+		POPFRAME\n\
+		RETURN\n\
+	\n\
+	LABEL $strval\n\
+		PUSHFRAME\n\
+		DEFVAR LF@$strval$return\n\
+		MOVE LF@$strval$return LF@%0\n\
+		TYPE GF@tmp1 LF@%0\n\
+		JUMPIFEQ $strval$type$nil GF@tmp1 string@nil\n\
+		JUMP $strval$end\n\
+		LABEL $strval$type$nil\n\
+		MOVE LF@$strval$return string@ \n\
+		LABEL $strval$end\n\
+		POPFRAME\n\
+		RETURN\n\
+	\n\
+	LABEL $strlen\n\
+		PUSHFRAME\n\
+		DEFVAR LF@$strlen$return\n\
+		STRLEN LF@$strlen$return LF@%0\n\
+		POPFRAME\n\
+		RETURN\n\
+	\n\
+	LABEL $substring\n\
+		PUSHFRAME\n\
+		DEFVAR LF@$substring$return\n\
+		MOVE LF@$substring$return string@ \n\
+		DEFVAR LF@$substring$control\n\
+		LT LF@$substring$control LF@%1 int@0\n\
+		JUMPIFEQ $substring$end LF@$substring$control bool@true\n\
+		LT LF@$substring$control LF@%2 int@0\n\
+		JUMPIFEQ $substring$end LF@$substring$control bool@true\n\
+		GT LF@$substring$control LF@%1 LF@%2\n\
+		JUMPIFEQ $substring$end LF@$substring$control bool@true\n\
+		DEFVAR LF@$substring$len\n\
+		STRLEN LF@$substring$len LF@%0\n\
+		GT LF@$substring$control LF@%1 LF@$substring$len\n\
+		JUMPIFEQ $substring$end LF@$substring$control bool@true\n\
+		GT LF@$substring$control LF@%2 LF@$substring$len\n\
+		JUMPIFEQ $substring$end LF@$substring$control bool@true\n\
+		DEFVAR LF@$substring$char\n\
+		LABEL $substring$loop\n\
+		GETCHAR LF@$substring$char LF@%0 LF@%1\n\
+		CONCAT LF@$substring$return LF@$substring$char\n\
+		LT LF@$substring$control LF@%1 LF@%2\n\
+		JUMPIFEQ $substring$loop LF@$substring$control bool@true\n\
+		LABEL $substring$end\n\
+		POPFRAME\n\
+		RETURN\n\
+	\n\
+	LABEL $ord\n\
+		PUSHFRAME\n\
+		DEFVAR LF@$ord$return\n\
+		MOVE LF@$ord$return int@0\n\
+		DEFVAR LF@$ord$len\n\
+		STRLEN LF@$ord$len LF%0\n\
+		JUMPIFEQ $ord$end LF@$ord$len int@0\n\
+		STRI2INT LF@$ord$return LF%0 int@0\n\
+		LABLE $ord$end\n\
+		POPFRAME\n\
+		RETURN\n\
+	\n\
+	LABLE $chr\n\
+		PUSHFRAME\n\
+		DEFVAR LF@$chr$return\n\
+		INT2CHAR LF@$chr$return LF@%0\n\
+		POPFRAME\n\
+		RETURN\n\
 	");
 }
 
